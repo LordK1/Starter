ca76c3473e31f1f87f79c73090e8146cbd0fb6cc
LordK1 <keyvan.ghanizadeh@gmail.com>
2015-08-26 11:18:12 +0430

    Templates updated

--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -1,5 +1,8 @@
 from .base import *  # NOQA
 
+"""
+ Note: When you are developing on {{ project_name }} should use this settings on manage.py
+"""
 # Django Debug Toolbar
 INSTALLED_APPS += (
     'debug_toolbar',
