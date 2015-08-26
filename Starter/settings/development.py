187f4bf2e5a97e66bd0a242ac8aee4d077ec62eb
LordK1 <keyvan.ghanizadeh@gmail.com>
2015-08-26 10:54:04 +0430

    tmp added to requirements

--- Starter/settings/development.py
+++ Starter/settings/development.py
@@ -3,9 +3,8 @@ from .base import *  # NOQA
 # Django Debug Toolbar
 INSTALLED_APPS += (
     'debug_toolbar',
-    'debug_panel',
 )
 # Django Debug Panel Middleware
 MIDDLEWARE_CLASSES += (
-    'debug_panel.middleware.DebugPanelMiddleware',
+    'debug_toolbar.base.',
 )
